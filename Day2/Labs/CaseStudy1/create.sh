bbf50997452ab9fea977ff765973ba3b65e7e482
torosent <torosent@microsoft.com>
2017-10-05 09:50:40 +0300

    updated case studies  and wordpress template

--- Day2/Labs/CaseStudy1/create.sh
+++ Day2/Labs/CaseStudy1/create.sh
@@ -10,5 +10,8 @@ kubectl create -f secret.yaml
 # Deploy the Event Hub processors
 kubectl create -f eph.yaml
 
+# Define autoscale
+kubectl autoscale deployment eph --cpu-percent=50 --min=1 --max=10
+
 # Simulate cars with event hub loader from local machine with messages and see them in CosmosDB
 docker run --rm -it -e NAMESPACE=<namespace> -e EH_NAME=<eh_name> -e SAS_KEY_NAME=<sas_key_name> -e SAS_KEY <sas_key>  torosent/ehsender
