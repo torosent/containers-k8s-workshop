508ffa3228260e78ad2cb9ef323615a5a60fbf3f
torosent <torosent@microsoft.com>
2017-10-23 08:39:32 +0300

    updated microservices example

--- Day2/Demos/Kubernetes/Microservices/StickerStore/Deployment/commands.sh
+++ Day2/Demos/Kubernetes/Microservices/StickerStore/Deployment/commands.sh
@@ -1,5 +1,17 @@
 #! /bin/sh
 
+
+# build containers
+docker build -t torosent/webstore:1.0.0 .
+docker build -t torosent/statusservice:1.0.0 .
+docker build -t torosent/printingservice:1.0.0 .
+docker build -t torosent/orderservice:1.0.0 .
+
+docker push torosent/webstore:1.0.0
+docker push torosent/printingservice:1.0.0
+docker push torosent/statusservice:1.0.0
+docker push torosent/orderservice:1.0.0
+
 # create a secert
 kubectl create -f secret.yaml
 
@@ -16,8 +28,9 @@ kubectl scale --replicas 1 deployment printingservice-deployment
 # cd into Printing service folder
 # change the version number in printer.cs line 68
 
-docker build -t torosent/printingservice:1.0.8 . 
-docker push torosent/printingservice:1.0.8
-kubectl set image deployment/printingservice-deployment printingservice=torosent/printingservice:1.0.8
-
+docker build -t torosent/printingservice:1.0.1 . 
+docker push torosent/printingservice:1.0.1
+kubectl set image deployment/printingservice-deployment printingservice=torosent/printingservice:1.0.1
+kubectl rollout history deployment/printingservice-deployment
+kubectl rollout undo deployment/printingservice-deployment --to-revision=1
  
