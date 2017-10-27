616f7328eaa3025b918c7cd42b8e411b1f89ae6b
João Almeida <joao.diogo.almeida@gmail.com>
2017-10-27 18:45:52 +0100

    Day 1 Review - Docker Storage

--- Day1/Demos/Docker-Storage/mongodb.cmd
+++ Day1/Demos/Docker-Storage/mongodb.cmd
@@ -3,8 +3,5 @@ docker run --name mongodb -d mongo
 docker ps -a 
 docker rm mongodb
 
-@REM With external storage
-docker run --name mongodb -v /Users/torosent/MongoDBData:/data/db -d mongo
-
 @REM windows
 docker run --name mongodb -v /c/MongoDBData:/data/db -d mongo
