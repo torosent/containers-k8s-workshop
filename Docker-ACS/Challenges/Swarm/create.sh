ffff77cd9539cc433efa020e688c27d8100e899a
Stewart Adam <s.adam@diffingo.com>
2017-11-15 17:04:36 -0800

    Source common variables, do not use static RG and resource names

--- Docker-ACS/Challenges/Swarm/create.sh
+++ Docker-ACS/Challenges/Swarm/create.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
-rg=swarmworkshop
-acsname=mySwarmCluster
+. ../../../common.vars
+RG="${PREFIX}-swarmworkshop"
+ACSNAME="${PREFIX}-mySwarmCluster"
 
 # create resource group
-az group create --name  $rg --location ukwest //Swarm mode region
+az group create --name "$RG" --location ukwest //Swarm mode region
 
 # create swarm with generate keys
-# az acs create --orchestrator-type DockerCE --resource-group $rg --name  $acsname --generate-ssh-keys --agent-count 1
+# az acs create --orchestrator-type DockerCE --resource-group "$RG" --name "$ACSNAME" --generate-ssh-keys --agent-count 1
 
 # create swarm with predefined keys
-az acs create --orchestrator-type DockerCE --resource-group $rg --name  $acsname --ssh-key-value ~/.ssh/id_rsa.pub --agent-count 1 
+az acs create --orchestrator-type DockerCE --resource-group "$RG" --name "$ACSNAME" --ssh-key-value "$SSH_PUB_KEY" --agent-count 1
--- Kubernetes/GettingStarted/setup-acs.sh
+++ Kubernetes/GettingStarted/setup-acs.sh
@@ -1,17 +1,38 @@
 #! /bin/sh
+. ../../common.vars
+RG="${PREFIX}-kubeworkshop"
+NAME="${PREFIX}-kubecluster"
 
 # Creat Resource Group
-az group create --name myK8sCluster --location westeurope
+az group create --name "$NAME" --location "$LOCATION"
 
-# Create Cluster
-#az acs create --orchestrator-type kubernetes --resource-group myK8sCluster --name myK8sCluster --service-principal <client_id> --client-secret <client_secret> --agent-vm-size Standard_DS3_v2 --agent-count 2 --generate-ssh-keys
-az acs create --orchestrator-type kubernetes --resource-group myK8sCluster --name myK8sCluster --service-principal <client_id> --client-secret <client_secret> --agent-vm-size Standard_DS3_v2 --agent-count 2 --ssh-key-value ~/.ssh/id_rsa.pub
+
+# If you do not have an SSH keys, replace --ssh-key-value "$SSH_PUB_KEY" with --generate-ssh-keys
+
+## START IF ##
+## OPTION A: Create Cluster - if you are an Azure sub owner
+
+az acs create --orchestrator-type kubernetes --resource-group "$RG" --name "$NAME" --agent-vm-size Standard_DS3_v2 --agent-count 2 --ssh-key-value "$SSH_PUB_KEY"
+
+## OPTION B: Create Cluster - if you are NOT an Azure sub owner
+chmod +x "obtainAzureSP.sh"
+./obtainAzureSP.sh
+
+AZURE_CLIENT_ID="COPY_ME_FROM_obtainAzureSP_OUTPUT"
+AZURE_CLIENT_SECRET="COPY_ME_FROM_obtainAzureSP_OUTPUT"
+
+az acs create --orchestrator-type kubernetes --resource-group "$RG" --name "$NAME" --agent-vm-size Standard_DS3_v2 --agent-count 2 --ssh-key-value "$SSH_PUB_KEY" --service-principal "$AZURE_CLIENT_ID" --client-secret "$AZURE_CLIENT_SECRET"
+
+## ENDIF ##
 
 # Must for first time only ; Install Kubectl CLI. If you are using Windows than kubectl is in program files (x86). Make sure it is in your PATH variable
 az acs kubernetes install-cli
 
 # Connect kubectl to cluster
-az acs kubernetes get-credentials --resource-group=myK8sCluster --name=myK8sCluster
+az acs kubernetes get-credentials --resource-group="$RG" --name="$NAME" --ssh-key-file "$SSH_PRIV_KEY"
 
 # Proxy to the dashboard
 kubectl proxy
+
+# Now browse to
+# http://127.0.0.1:8001/ui
