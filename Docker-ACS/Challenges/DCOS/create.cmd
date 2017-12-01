ffae7c203922bb19f08fef6bb3ad63a99ec25c9d
Jennelle Crothers <jkcrothers@hotmail.com>
2017-12-01 14:30:59 -0800

    removed extra space

--- Docker-ACS/Challenges/DCOS/create.cmd
+++ Docker-ACS/Challenges/DCOS/create.cmd
@@ -9,7 +9,7 @@ az group create --name  %rg% --location westus
 
 @REM Create cluster with either auto-generated keys or provide your own.
 @REM Ootion 1: create dcos with generate keys
-az acs create --orchestrator-type dcos --resource-group %rg% --name  %acsname% --generate-ssh-keys --agent-count 1
+az acs create --orchestrator-type dcos --resource-group %rg% --name %acsname% --generate-ssh-keys --agent-count 1
 
 @REM Option 2: create dcos with predefined keys
 @REM https://docs.microsoft.com/en-us/azure/virtual-machines/linux/ssh-from-windows
