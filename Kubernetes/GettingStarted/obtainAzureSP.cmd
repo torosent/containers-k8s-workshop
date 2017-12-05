594f7a7280014530ed66aa5979e0455a3a1360eb
Jennelle Crothers <jkcrothers@hotmail.com>
2017-12-05 13:18:00 -0800

    Updated AKS and SP instructions

---
+++ Kubernetes/GettingStarted/obtainAzureSP.cmd
@@ -0,0 +1,4 @@
+az ad sp create-for-rbac --skip-assignment
+
+@REM Take note of the "appID" and the "password" from the command output.
+@REM Continue with the instructions from setup-acs.sh or setup-aks.sh using the Azure CLI from CMD.
--- Kubernetes/GettingStarted/setup-aks.sh
+++ Kubernetes/GettingStarted/setup-aks.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
+# LOCATION NOTE: AKS is not yet available in all regions. It's recommended to check the region availability before indicating the location of your resource group. https://azure.microsoft.com/en-us/regions/services/
 
 # Creat Resource Group
-az group create --name myK8sCluster --location westus2
+az group create --name myK8sCluster --location centralus
 
 # Create Cluster
 #az aks create --resource-group myK8sCluster --name myK8sCluster --service-principal <client_id> --client-secret <client_secret> --agent-vm-size Standard_DS3_v2 --agent-count 2 --generate-ssh-keys -k 1.8.1
-az aks create --resource-group myK8sCluster --name myK8sCluster --service-principal <client_id> --client-secret <client_secret> --agent-vm-size Standard_DS3_v2 --agent-count 2 --ssh-key-value ~/.ssh/id_rsa.pub -k 1.8.1
+az aks create --resource-group myK8sCluster --name myK8sCluster --service-principal <appID> --client-secret <password> --agent-vm-size Standard_DS3_v2 --agent-count 2 --ssh-key-value ~/.ssh/id_rsa.pub -k 1.8.1
 
 # Must for first time only ; Install Kubectl CLI. If you are using Windows than kubectl is in program files (x86). Make sure it is in your PATH variable
 az aks install-cli
