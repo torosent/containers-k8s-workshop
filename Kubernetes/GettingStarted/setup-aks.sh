4e6a868f047aaf53a5f2e5d3d830822756891fc3
Stephanie Marker <Stephanie.Marker93@gmail.com>
2017-12-13 00:21:21 -0500

    Change agent to node. (#23)

--- Kubernetes/GettingStarted/setup-aks.sh
+++ Kubernetes/GettingStarted/setup-aks.sh
@@ -5,8 +5,8 @@
 az group create --name myK8sCluster --location centralus
 
 # Create Cluster
-#az aks create --resource-group myK8sCluster --name myK8sCluster --service-principal <client_id> --client-secret <client_secret> --agent-vm-size Standard_DS3_v2 --agent-count 2 --generate-ssh-keys -k 1.8.1
-az aks create --resource-group myK8sCluster --name myK8sCluster --service-principal <appID> --client-secret <password> --agent-vm-size Standard_DS3_v2 --agent-count 2 --ssh-key-value ~/.ssh/id_rsa.pub -k 1.8.1
+#az aks create --resource-group myK8sCluster --name myK8sCluster --service-principal <client_id> --client-secret <client_secret> --node-vm-size Standard_DS3_v2 --node-count 2 --generate-ssh-keys -k 1.8.1
+az aks create --resource-group myK8sCluster --name myK8sCluster --service-principal <appID> --client-secret <password> --node-vm-size Standard_DS3_v2 --node-count 2 --ssh-key-value ~/.ssh/id_rsa.pub -k 1.8.1
 
 # Must for first time only ; Install Kubectl CLI. If you are using Windows than kubectl is in program files (x86). Make sure it is in your PATH variable
 az aks install-cli
