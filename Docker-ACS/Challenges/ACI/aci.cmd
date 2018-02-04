c4cadffea07c7ad55d8122496ed91cab2352442c
Tomer Rosenthal <torosent@microsoft.com>
2018-02-04 23:15:54 +0800

    Update aci.cmd

--- Docker-ACS/Challenges/ACI/aci.cmd
+++ Docker-ACS/Challenges/ACI/aci.cmd
@@ -2,11 +2,7 @@
 az group create --name k8-lab --location canadacentral
 
 @REM create aci 
-<<<<<<< HEAD
-az container create --name mycontain0er --image tutum/hello-world --resource-group k8-lab --ip-address public --port 80
-=======
 az container create --name mycontainer --image tutum/hello-world --resource-group myResourceGroup --ip-address public --port 80
->>>>>>> origin/master
 
 @REM show details and public ip
 az container show --name mycontainer --resource-group k8-lab
